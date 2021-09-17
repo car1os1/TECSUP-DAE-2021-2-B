5133b5f16702f84608b289767899485d005ef05d
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2021-09-16 20:15:31 -0500

    1

--- lab05/tienda/models.py
+++ lab05/tienda/models.py
@@ -27,6 +27,7 @@ class Cliente(models.Model):
     direccion = models.CharField(max_length =200)
     email = models.EmailField(max_length=200)
 
-
+    def __str__(self):
+        return self.nombres
 
     
