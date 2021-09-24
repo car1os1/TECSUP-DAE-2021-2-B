43a2bb01a5ae0024e2b995d0c1c9b66e7cdc8a14
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2021-09-23 22:39:07 -0500

    lab06

---
+++ lab06/tienda/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Producto
+
+# Create your views here.
+def index(request):
+    product_list = Producto.objects.order_by('nombre')[:6]
+    context= {'product_list' : product_list}
+    return render(request, 'index.html', context) 
+
+def producto(request, producto_id):
+    producto = get_object_or_404(Producto, pk=producto_id)
+    return render(request, 'producto.html', {'producto': producto})    
