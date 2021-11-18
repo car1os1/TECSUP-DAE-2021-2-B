5a9a45f98767fcf30fd64bf1dba18528832393b4
car1os1 <carlos.chullunquia@tecsup.edu.pe>
2021-11-18 11:57:29 -0500

    lab13

--- backend/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from rest_framework import serializers
-from .models import prestamo
-
-
-class PrestamoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = prestamo
-        fields = '__all__'
